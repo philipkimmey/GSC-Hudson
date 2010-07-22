f7b8e7eb10496180faffdef6de671ea23efe6b7c
pkimmey <pkimmey@genome.wustl.edu>
2010-07-22 16:17:56 -0500

    begin use of defaults.pl to store options for deploy.pl

--- deploy/defaults.pl
+++ deploy/defaults.pl
@@ -1,1 +1,9 @@
 # set defaults in here.
+our $RSS_FEED_URL = 'http://linus262:8080/job/Genome/rssAll';
+our $BUILD_PATH = '/gscuser/pkimmey/.hudson/jobs/Genome/builds';
+our $SNAPSHOT_PATH = '/gscuser/pkimmey/.snapshot';
+our $GSCPAN = $ENV{GSCPAN} || 'svn+ssh://svn/srv/svn/gscpan';
+
+our $UR_REPOSITORY = 'git://github.com/sakoht/UR.git';
+our $WORKFLOW_REPOSITORY = 'ssh://git/srv/git/workflow.git';
+our $GENOME_REPOSITORY = 'ssh://git/srv/git/genome.git';
